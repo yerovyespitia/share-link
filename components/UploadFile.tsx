327aa2fa5647f3e64fa00a0d9e5dc69b806d2a0d
Yerovy Espitia <luyeesgar@gmail.com>
2025-08-09 12:05:56 -0500

    style: add animations

--- components/UploadFile.tsx
+++ components/UploadFile.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { motion } from 'framer-motion'
 
 import { Input } from './ui/input'
+import { Upload } from 'lucide-react'
 
 export const UploadFile = () => {
   const [fileName, setFileName] = useState<string>('No file selected')
@@ -25,12 +26,17 @@ export const UploadFile = () => {
           {fileName}
         </motion.p>
       )}
-      <label
+      <motion.label
         htmlFor='file'
-        className='bg-primary text-primary-foreground rounded-full font-medium px-5 py-2 cursor-pointer inline-block text-center'
+        className='bg-primary text-primary-foreground rounded-full font-semibold px-5 py-3 cursor-pointer inline-block text-center'
+        whileTap={{ scale: 0.95 }}
+        transition={{ duration: 0.5 }}
       >
-        {fileName === 'No file selected' ? 'Choose a file' : 'Change file'}
-      </label>
+        <span className='flex items-center gap-2'>
+          <Upload size={17} />
+          {fileName === 'No file selected' ? 'Choose a file' : 'Change file'}
+        </span>
+      </motion.label>
       <span className='text-sm text-muted-foreground'>{fileName}</span>
       <Input
         className='hidden'
